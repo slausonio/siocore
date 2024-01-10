00033f30b58316790301f4a0886a713dc4ff22d6
Matthew Slauson <matthew@slauson.io>
2024-01-09 23:56:00 -0600

    add

---
+++ log/loki.go
@@ -0,0 +1,20 @@
+package log
+
+import (
+	"github.com/grafana/loki-client-go/loki"
+	slogloki "github.com/samber/slog-loki/v3"
+	"github.com/slausonio/siocore"
+	"log/slog"
+)
+
+func NewLokiClient(env siocore.Env) (*slog.Logger, *loki.Client) {
+	config, _ := loki.NewDefaultConfig(env.Value(siocore.EnvKeyLokiHost))
+	config.TenantID = "xyz"
+	client, _ := loki.New(config)
+
+	logger := slog.New(slogloki.Option{Level: slog.LevelDebug, Client: client}.NewLokiHandler())
+	logger = logger.
+		With("env", env.Value(siocore.EnvKeyCurrentEnv))
+
+	return logger, client
+}
